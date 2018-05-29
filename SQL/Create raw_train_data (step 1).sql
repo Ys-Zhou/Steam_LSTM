8ffdada99cbd84eeea46e5dcd7df81f20d517efb
Ys-Zhou <zhouyan1993ys@gmail.com>
2018-05-29 20:12:49 +0900

    Y. Zhou

--- SQL/Create raw_train_data (step 1).sql
+++ SQL/Create raw_train_data (step 1).sql
@@ -1,12 +1,30 @@
 INSERT INTO raw_train_data (userid, gameid)
-SELECT userid, gameid FROM date170416
-UNION
-SELECT userid, gameid FROM date170430
-UNION
-SELECT userid, gameid FROM date170514
-UNION
-SELECT userid, gameid FROM date170528
-UNION
-SELECT userid, gameid FROM date170611
-UNION 
-SELECT userid, gameid FROM date170625
+  SELECT
+    userid,
+    gameid
+  FROM date170416
+  UNION
+  SELECT
+    userid,
+    gameid
+  FROM date170430
+  UNION
+  SELECT
+    userid,
+    gameid
+  FROM date170514
+  UNION
+  SELECT
+    userid,
+    gameid
+  FROM date170528
+  UNION
+  SELECT
+    userid,
+    gameid
+  FROM date170611
+  UNION
+  SELECT
+    userid,
+    gameid
+  FROM date170625;
--- SQL/Create raw_train_data (step 2).sql
+++ SQL/Create raw_train_data (step 2).sql
@@ -1,3 +1,3 @@
 UPDATE raw_train_data AS r, date170416 AS d
 SET r.d1 = d.twoweeks
-WHERE r.userid = d.userid AND r.gameid = d.gameid
+WHERE r.userid = d.userid AND r.gameid = d.gameid;
