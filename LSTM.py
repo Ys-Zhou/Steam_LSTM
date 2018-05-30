61b40ce0b608474763267e1c19a3b0575dcb0b99
Ys-Zhou <zhouyan1993ys@gmail.com>
2018-05-30 23:05:23 +0900

    Y. Zhou

--- LSTM.py
+++ LSTM.py
@@ -4,14 +4,13 @@ from time import time
 from DBConnector import GetCursor
 
 # LSTM constants
-hidden_unit = 256
-data_size = 7649
-embedding_size = 64
-input_size = output_size = embedding_size
+hidden_unit = 16
+input_size = output_size = 7649
 
 # Training parameters
 lr = 0.0005  # Learning rate
-ls = 100  # Learning step
+max_ls = 200  # Max learning step
+end_cnd = 0.999  # End condition (RMSE)
 time_step = 8
 batch_size = 128
 X = tf.placeholder(tf.float32, shape=[None, time_step, input_size])
@@ -110,65 +109,50 @@ def get_test():
     return game_list, test_x, test_y, known
 
 
-# Summery variables
-def variable_summaries(var):
-    with tf.name_scope('summaries'):
-        # Record mean (avg)
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean', mean)
-        # Record stddev
-        with tf.name_scope('stddev'):
-            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev', stddev)
-        # Record max and min
-        tf.summary.scalar('max', tf.reduce_max(var))
-        tf.summary.scalar('min', tf.reduce_min(var))
-        # Record distribution
-        tf.summary.histogram('histogram', var)
-
-
 def lstm(batch: int):
-    with tf.name_scope('embedding_layer'):
-        pass
-    # embedding = tf.Variable(tf.random_uniform([data_size, embedding_size], -1.0, 1.0))
-    # embed = tf.nn.embedding_lookup(embedding, X)
     with tf.name_scope('input_layer'):
         with tf.name_scope('input_weights'):
             w_in = tf.Variable(tf.random_normal([input_size, hidden_unit]))
+            tf.summary.histogram('input_weights', w_in)
         with tf.name_scope('input_biases'):
             b_in = tf.Variable(tf.constant(0.1, shape=[hidden_unit, ]))
+            tf.summary.histogram('input_biases', b_in)
         with tf.name_scope('input'):
             # Reshape tensor to 2-dimension for matrix multiplication
-            input_ = tf.reshape(X, [-1, input_size])
-            input_rnn = tf.matmul(input_, w_in) + b_in
+            # (b, s, in) => (b * s, in)
+            input_x = tf.reshape(X, [-1, input_size])
+            # Linear map
+            # (b * s, in) => (b * s, h)
+            input_rnn = tf.matmul(input_x, w_in) + b_in
             # Reshape tensor back to 3-dimension
+            # (b * s, h) => (b, s, h)
             input_rnn = tf.reshape(input_rnn, [-1, time_step, hidden_unit])
-    with tf.name_scope('cell'):
+    with tf.name_scope('lstm_cell'):
         cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_unit)
         init_state = cell.zero_state(batch, dtype=tf.float32)
         # output_rnn: results of every LSTM cells
         # states: states in the last LSTM cell
         output_rnn, states = tf.nn.dynamic_rnn(cell, input_rnn, initial_state=init_state, dtype=tf.float32)
     with tf.name_scope('output_layer'):
-        # Reshape tensor to 2-dimension for matrix multiplication
-        output = tf.reshape(output_rnn, [-1, hidden_unit])
         with tf.name_scope('output_weights'):
             w_out = tf.Variable(tf.random_normal([hidden_unit, output_size]))
-            variable_summaries(w_out)
+            tf.summary.histogram('output_weights', w_out)
         with tf.name_scope('output_biases'):
             b_out = tf.Variable(tf.constant(0.1, shape=[output_size, ]))
-            variable_summaries(b_out)
-        with tf.name_scope('prediction'):
-            pred = tf.matmul(output, w_out) + b_out
-            variable_summaries(pred)
-    return pred, states
+            tf.summary.histogram('output_biases', b_out)
+        with tf.name_scope('output'):
+            # (b, s, h) => (b * s, h)
+            output_rnn = tf.reshape(output_rnn, [-1, hidden_unit])
+            # (b * s, h) => (b * s, out)
+            output_y = tf.matmul(output_rnn, w_out) + b_out
+    return output_y, states
 
 
 def train():
     train_x, train_y = get_data()
     # Run time
     time_start = time()
-    pred, states = lstm(batch_size)
+    pred, _ = lstm(batch_size)
     # loss and evaluation function
     with tf.name_scope('loss'):
         loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1]) - tf.reshape(Y, [-1])))
@@ -185,18 +169,21 @@ def train():
         # initialize or reload model
         sess.run(tf.global_variables_initializer())
         # saver.restore(sess, module_file)
-        # Training times
-        for turn in range(ls):
-            loss_ = None
+        last_loss = 9999
+        curr_loss = 999
+        turn = 0
+        while turn < max_ls and curr_loss / last_loss < end_cnd:
+            last_loss = curr_loss
+            turn += 1
             feed_dict = {}
             for start in range(0, len(train_x) - batch_size, batch_size):
                 feed_dict = {X: train_x[start:start + batch_size], Y: train_y[start:start + batch_size]}
-                states, loss_ = sess.run([train_op, loss], feed_dict=feed_dict)
+                _, curr_loss = sess.run([train_op, loss], feed_dict=feed_dict)
             # Write summaries
             summary = sess.run(merged, feed_dict=feed_dict)
             summary_writer.add_summary(summary, global_step=turn)
-            print('Step %d: loss = %f' % (turn, loss_))
-        saver.save(sess, 'models/NoSVD_%d/NoSVD.model' % hidden_unit, global_step=ls)
+            print('Step %d: current loss = %g (%g)' % (turn, curr_loss, curr_loss / last_loss))
+        saver.save(sess, 'models/NoSVD_%d/NoSVD.model' % hidden_unit, global_step=turn)
         time_stop = time()
         print(time_stop - time_start)
 
@@ -224,6 +211,6 @@ def prediction():
 
 
 if __name__ == '__main__':
-    # train()
+    train()
     # prediction()
     pass
