5876bd15cf793f263783324b0588013aa8e019bb
Ys-Zhou <zhouyan1993ys@gmail.com>
2018-06-01 19:42:05 +0900

    Y. Zhou

--- models.py
+++ models.py
@@ -3,47 +3,42 @@ import tensorflow as tf
 
 class Models:
     @staticmethod
-    def lstm(input_size: int, hidden_unit: int, output_size: int, time_step: int, batch_size: int):
-        with tf.name_scope('input'):
-            input_x = tf.placeholder(tf.float32, shape=[None, time_step, input_size])
-        with tf.name_scope('input layer'):
+    def lstm(input_x: tf.Tensor, input_size: int, hidden_unit: int, output_size: int, time_step: int, batch_size: int):
+        with tf.name_scope('input_layer'):
             with tf.name_scope('weights'):
                 w_in = tf.Variable(tf.random_normal([input_size, hidden_unit]))
                 tf.summary.histogram('weights', w_in)
             with tf.name_scope('biases'):
                 b_in = tf.Variable(tf.constant(0.1, shape=[hidden_unit, ]))
                 tf.summary.histogram('biases', b_in)
-            with tf.name_scope('full connection'):
-                # Reshape tensor to 2-dimension for matrix multiplication
-                # (b, s, in) => (b * s, in)
-                input_x = tf.reshape(input_x, [-1, input_size])
-                # linear map
-                # (b * s, in) => (b * s, h)
-                input_rnn = tf.matmul(input_x, w_in) + b_in
-                # Reshape tensor back to 3-dimension
-                # (b * s, h) => (b, s, h)
-                input_rnn = tf.reshape(input_rnn, [-1, time_step, hidden_unit])
-        with tf.name_scope('lstm units'):
+            # Reshape tensor to 2-dimension for matrix multiplication
+            # (b, s, in) => (b * s, in)
+            input_x = tf.reshape(input_x, [-1, input_size])
+            # linear map
+            # (b * s, in) => (b * s, h)
+            input_rnn = tf.matmul(input_x, w_in) + b_in
+            # Reshape tensor back to 3-dimension
+            # (b * s, h) => (b, s, h)
+            input_rnn = tf.reshape(input_rnn, [-1, time_step, hidden_unit])
+        with tf.name_scope('lstm_units'):
             cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_unit)
             init_state = cell.zero_state(batch_size, dtype=tf.float32)
             # output_rnn: results in each step
             # last_states: final states
             output_rnn, last_states = tf.nn.dynamic_rnn(cell, input_rnn, initial_state=init_state, dtype=tf.float32)
-        with tf.name_scope('output layer'):
+        with tf.name_scope('relu_layer'):
             with tf.name_scope('weights'):
                 w_out = tf.Variable(tf.random_normal([hidden_unit, output_size]))
                 tf.summary.histogram('weights', w_out)
             with tf.name_scope('biases'):
                 b_out = tf.Variable(tf.constant(0.1, shape=[output_size, ]))
                 tf.summary.histogram('biases', b_out)
-            with tf.name_scope('full connection'):
-                # (b, s, h) => (b * s, h)
-                output_rnn = tf.reshape(output_rnn, [-1, hidden_unit])
-                # (b * s, h) => (b * s, out)
-                output_y = tf.matmul(output_rnn, w_out) + b_out
+            # (b, s, h) => (b * s, h)
+            output_rnn = tf.reshape(output_rnn, [-1, hidden_unit])
+            # (b * s, h) => (b * s, out)
+            output_relu = tf.nn.relu_layer(output_rnn, w_out, b_out)
+        with tf.name_scope('softmax'):
+            output_y = tf.nn.softmax(output_relu)
+            # (b * s, out) => (b, s, out)
+            output_y = tf.reshape(output_y, [-1, time_step, output_size])
         return output_y
-
-    @staticmethod
-    def codec():
-        with tf.name_scope('input'):
-            input_x = tf.placeholder(tf.float32, shape=[None, time_step, input_size])
--- train.py
+++ train.py
@@ -4,85 +4,103 @@ from models import Models
 
 # LSTM parameters
 input_size = output_size = 7649
-hidden_unit = 16
 time_step = 8
 batch_size = 128
 
 # training parameters
-learning_rate = 0.0005
-max_ls = 200  # Max learning step
-end_cnd = 0.999  # End condition (RMSE)
+learning_rate = 0.0001
+max_ls = 200  # max learning step
 
 # other parameters
 length_ts = 12  # Time series length
 user_limit = 2000
 
 
-def train():
+def train(hidden_unit: int):
     # placeholder and data set
     x = tf.placeholder(tf.float32, shape=[None, time_step, input_size])
     y = tf.placeholder(tf.float32, shape=[None, time_step, output_size])
     train_x, train_y = DataSet(time_step, user_limit).get_data()
+
     # Load model
-    prd = Models.lstm(input_size, hidden_unit, output_size, time_step, batch_size)
-    # loss and evaluation function
-    with tf.name_scope('loss'):
-        loss = tf.reduce_mean(tf.square(tf.reshape(prd, [-1]) - tf.reshape(y, [-1])))
-        tf.summary.scalar('loss', loss)
+    prd = Models.lstm(x, input_size, hidden_unit, output_size, time_step, batch_size)
+
+    # error and evaluation function
     with tf.name_scope('train'):
-        train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        error = tf.reduce_mean(tf.abs(tf.subtract(prd, y)))
+        tf.summary.scalar('error', error)
+        update_op = tf.train.AdamOptimizer(learning_rate).minimize(error)
+
     # initialize or reload model
     saver = tf.train.Saver(tf.global_variables())
     # module_file = tf.train.latest_checkpoint()
-    with tf.Session() as sess:
+
+    # Run session
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
+
         # Merge summaries
         merged = tf.summary.merge_all()
         summary_writer = tf.summary.FileWriter('models/NoSVD_%d/log' % hidden_unit, sess.graph)
+
         # initialize or reload model
         sess.run(tf.global_variables_initializer())
         # saver.restore(sess, module_file)
-        last_loss = 9999
-        crrn_loss = 999
-        turn = 0
-        while turn < max_ls and crrn_loss / last_loss < end_cnd:
-            last_loss = crrn_loss
-            turn += 1
+
+        # Start learning
+        curr_err = 1
+        for turn in range(max_ls):
+            last_err = curr_err
             feed_dict = {}
             for start in range(0, len(train_x) - batch_size, batch_size):
                 feed_dict = {x: train_x[start:start + batch_size], y: train_y[start:start + batch_size]}
-                _, crrn_loss = sess.run([train_op, loss], feed_dict=feed_dict)
+                _, curr_err = sess.run([update_op, error], feed_dict=feed_dict)
+
             # Write summaries
             summary = sess.run(merged, feed_dict=feed_dict)
             summary_writer.add_summary(summary, global_step=turn)
-            print('Step %d: current loss = %g (%g)' % (turn, crrn_loss, crrn_loss / last_loss))
-        saver.save(sess, 'models/NoSVD_%d/NoSVD.model' % hidden_unit, global_step=turn)
+
+            print('Step %d: current error = %g (%g)' % (turn, curr_err, curr_err / last_err))
+
+        # Save model
+        saver.save(sess, 'models/NoSVD_%d/NoSVD.model' % hidden_unit, global_step=max_ls)
 
 
 # By Recall(Top-50)
-def prediction():
-    hits = 0
-    crrs = 0
+def prediction(hidden_unit: int):
     x = tf.placeholder(tf.float32, shape=[None, time_step, input_size])
     test_x, test_y, known = DataSet(time_step, user_limit).get_test()
-    prd = Models.lstm(input_size, hidden_unit, output_size, time_step, batch_size=1)
+
+    prd = Models.lstm(x, input_size, hidden_unit, output_size, time_step, batch_size=1)
+
     saver = tf.train.Saver(tf.global_variables())
+
+    hits = 0
+    crrs = 0
     with tf.Session() as sess:
         module_file = tf.train.latest_checkpoint('models/NoSVD_%d/' % hidden_unit)
         saver.restore(sess, module_file)
+
         for i in range(len(test_x)):
             next_ = sess.run(prd, feed_dict={x: [test_x[i]]})
-            priority = [[i, p] for p, i in enumerate(next_[-1])]
+
+            priority = [[i, p] for p, i in enumerate(next_[0][-1])]
             priority.sort(reverse=True)
             priority = list(map(list, zip(*priority)))[1]
+
             rec = [g for g in priority if g not in known[i]][:50]
             crr = [h for h in test_y[i] if h not in known[i]]
+
             hits += len(set(rec).intersection(set(crr)))
             crrs += len(crr)
+
             print('[hits, corrs] = [%d, %d]' % (hits, crrs))
+
     print('recall = %g' % (hits / crrs))
 
 
 if __name__ == '__main__':
-    # train()
-    prediction()
+    train(128)
+    # prediction(128)
     pass
